13f8b216ebaece26b7428ff1cddb4a2bd1d587b4
Guilherme Rosa <guilherme.rosa.santos@outlook.com>
2023-10-25 15:31:44 -0300

    Serialized

---
+++ com.com.rosa/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Poços de Caldas', 'Larissa', 'Female', 'Oliveira'),
+	(2, 'Aguaí', 'Guilherme', 'Male', 'Rosa'),
+	(5, 'India', 'Mahatma', 'Male', 'Gandhi');
